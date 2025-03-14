504dc70ca4da5a1af376fb0059a76f56bdcc577e
accazeta <f.panzeri24@studenti.unibg.it>
2025-03-14 16:05:55 +0100

    Revert "Added support for a different dataset (Denmark)"
    This reverts commit 681546e3a82d548b8cdc5a4e1bfa7d531188c00f.

--- src/Constants.py
+++ src/Constants.py
@@ -1,9 +1,7 @@
 import datetime as dt                           # datetime module for time intervals
 
 # constants
-FOLDER_PATH = "../dataset/"
-HAWAII = 'Hawaii/2019/'
-DENMARK = 'Denmark/'
+FOLDER_PATH = "../dataset/2019/"
 LOG_FILE_NAME = 'log_file.txt'
 SPEED_THRESHOLD = 0.5 # nautical knots
 ABSURD_SPEED_THRESHOLD = 102 # nautical knots
